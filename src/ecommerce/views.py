b766a2227fcef16ef2c7018dd804a4d02b3d453a
niawjunior <pasupol@dome.cloud>
2017-12-12 18:16:26 +0700

    templates context

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,4 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title":"Home Page!",
+        "content": "Welcome to the home page"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"About Page!",
+        "content": "Welcome to the About page"
+
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title":"Contact Page!",
+        "content": "Welcome to the contact page"
+
+    }
+    return render(request, "home_page.html", context)
