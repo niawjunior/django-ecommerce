b0fe876b68554d6e64e5b49d4677ba2024b1c790
niawjunior <pasupol@dome.cloud>
2017-12-17 18:40:45 +0700

    create model slug,featured,active

--- src/products/models.py
+++ src/products/models.py
@@ -8,18 +8,48 @@ def get_filename_ext(filepath):
     return name, ext
 
 def upload_image_path(instance, filename):
-    # print(instance)
-    # print(filename)
+
     new_filename = random.randint(1,23232335)
     name, ext = get_filename_ext(filename)
     final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
-    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
+    return "products/{new_filename}/{final_filename}".format(
+            new_filename=new_filename,
+            final_filename=final_filename
+            )
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+    def featured(self): #Product.objects.featured()
+        return self.get_queryset().featured()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id) #Product.objects == self.get_queryset()
+        if qs.count() ==1:
+            return qs.first()
+        return None
 
 class Product(models.Model):
     title       = models.CharField(max_length=120)
+    slug        = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image       = models.ImageField(upload_to=upload_image_path,null=True, blank=False)
+    featured    = models.BooleanField(default=False)
+    active      = models.BooleanField(default=True)
+
+
+    objects = ProductManager()
     def __str__(self):
         return self.title
 
