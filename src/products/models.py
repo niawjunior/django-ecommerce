db8c6030e719191ecd30c39aaf3b198e374ed19c
niawjunior <pasupol@dome.cloud>
2017-12-17 19:39:24 +0700

    import function pre_save,post_save and unique_slug_generator

--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,11 @@
 import random
 import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+
+
+from .utils import unique_slug_generator
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
@@ -44,7 +48,7 @@ class Product(models.Model):
     slug        = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image       = models.ImageField(upload_to=upload_image_path,null=True, blank=False)
+    image       = models.ImageField(upload_to=upload_image_path,null=True, blank=True)
     featured    = models.BooleanField(default=False)
     active      = models.BooleanField(default=True)
 
@@ -55,3 +59,10 @@ class Product(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
